4420f40b784c07aa6b6e29de1d140238c8255581
User <User@DESKTOP-M776RP5>
2022-10-16 19:39:57 +0800

    加入專案檔案。

---
+++ Exec_Factorial/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exec_Factorial
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("請輸入一個數字");
+			int num = Convert.ToInt32(Console.ReadLine());
+			int a = 1;
+			for (int i = 1; i <= num; i++)
+			{
+				a = a * i;
+			}
+			Console.WriteLine(num + "!為" + a);
+		}
+	}
+}
